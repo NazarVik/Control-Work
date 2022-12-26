9e4c08effeee25cfc4706cd8fd82101738962324
NazarVik <Nazarov_dev@icloud.com>
2022-12-26 18:17:38 +0300

    finished with task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] CreateStringArray()
+{
+    Console.Write("введите строки через пробел : ");
+    string[] strArray = Console.ReadLine().Split(" ");
+    return strArray; 
+}
+
+Console.Clear();
+string[] array = CreateStringArray();
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+        count++;
+}
+
+string[] newArray = new string[count];
+int index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        newArray[index] = array[i];
+        index++;
+    }
+}
+
+Console.WriteLine("[" + string.Join(", ", newArray) + "]");
